9dded167635f596101e6d0fb426ff19293b931ae
MartynasOnTop <ponasniekas10@gmail.com>
2024-02-29 18:13:01 +0200

    f

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,13 +1,44 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
+=======
+>>>>>>> b62f43cdf7dc82a421e22badfb225004aba4caf4
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
     public float speed = 20;
+<<<<<<< HEAD
 
     private void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
+=======
+    public GameObject explosionPrefab;
+
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        var health = other.gameObject.GetComponent<Health>();
+        if(health != null)
+        {
+            health.Damage(10);
+        }
+
+        Destroy(gameObject);
+        //transform.forward = other.contacts[0].normal;
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+    }
+}
+>>>>>>> b62f43cdf7dc82a421e22badfb225004aba4caf4
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+=======
+using UnityEngine;
+using UnityEngine.Events;
+>>>>>>> b62f43cdf7dc82a421e22badfb225004aba4caf4
 
 public class Weapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
 
+<<<<<<< HEAD
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -14,3 +20,88 @@ public class Weapon : MonoBehaviour
         }
     }
 }
+=======
+    public int ammo;
+    public int maxAmmo = 10;
+    public bool isReloading;
+    public bool isAutomatic;
+    public float fireInterval = 0.1f;
+    float fireCooldown;
+    public float reloadTime = 2;
+
+    public float bulletsPerShot;
+    public float spreadAngle = 5;
+    public int clipAmmo;
+    public int clipSIze;
+
+    public UnityEvent onRightCLick;
+    public UnityEvent onShoot;
+    public UnityEvent onReload;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            onRightCLick.Invoke();
+        }
+        if (!isAutomatic && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+        // automatic shooting
+        if (isAutomatic && Input.GetKey(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+        fireCooldown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        if(isReloading) return;
+        if (clipAmmo <= 0)
+        {
+            Reload();
+            return;
+        }
+        if(fireCooldown > 0) return;
+
+        onShoot.Invoke();
+
+        clipAmmo--;
+        fireCooldown = fireInterval;
+        for (int i = 0; i < bulletsPerShot; i++)
+        {
+            var bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            var offSetX = Random.RandomRange(-spreadAngle, +spreadAngle);
+            var offSetY = Random.RandomRange(-spreadAngle, +spreadAngle);
+            bullet.transform.eulerAngles += new Vector3(offSetX, offSetY, 0);
+        }
+    }
+
+    async void Reload()
+    {
+        if (clipAmmo == clipSIze) return;
+        if(isReloading) return;
+
+        isReloading = true;
+
+        onReload.Invoke();
+
+        print ("Reloading...");
+        await new WaitForSeconds(reloadTime);
+        print("Reloaded!");
+
+        isReloading = false;
+        //ammo = maxAmmo;
+        var ammoNeeded = Mathf.Min(clipSIze - clipAmmo, 1);
+        ammo -= ammoNeeded;
+        clipAmmo += ammoNeeded;
+    }
+}
+>>>>>>> b62f43cdf7dc82a421e22badfb225004aba4caf4
